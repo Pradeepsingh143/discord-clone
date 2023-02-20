41c11612177028af6f40e285bd2357cf9c5b7d28
pradeep <singhpardeep585@gmail.com>
2023-02-20 11:38:06 +0530

    added tailwind css package using node

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: '#404eed',
+      secondary: '#f6f6f6',
+      black: '#23272a',
+      white: '#fff',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
